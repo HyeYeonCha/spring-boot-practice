5a7d54efb6c743ae665200a744acd0e400b076a8
HyeYeonCha <gpdus335@naver.com>
2022-03-17 21:57:19 +0900

    sort service test 작성 #3

--- src/main/java/service/SortService.java
+++ src/main/java/service/SortService.java
@@ -7,9 +7,14 @@ import java.util.List;
 
 public class SortService {
 
+    private final Sort<String> sort;
+
+    public SortService(Sort<String> sort) {
+        this.sort = sort;
+        System.out.println("구현체 : " + sort.getClass().getName());
+    }
+
     public List<String> doSort(List<String> list) {
-        Sort<String> sort = new JavaSort<>();
-        
         return sort.sort(list);
     }
 }
