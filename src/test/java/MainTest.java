a7aa172f21b051599b8f82840c16b52436701ce9
HyeYeonCha <gpdus335@naver.com>
2022-03-17 21:33:32 +0900

    main 프로그램이 인자를 받아 정렬 기능 사용 #1

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -3,6 +3,10 @@ import org.junit.jupiter.api.Test;
 public class MainTest {
     @Test
     void main() {
-        Main.main(null);
+        // Given
+        String[] args = {"3", "1", "2"};
+
+        // When & Then
+        Main.main(args);
     }
 }
